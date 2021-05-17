57cd0eafc23d87cdffb60e5d4680d701df165c0a
galenhew <galenhew@gmail.com>
2021-05-18 03:21:13 +0800

    lead_detail views

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, Lead, Agent
+
+
+admin.site.register(User)
+admin.site.register(Lead)
+admin.site.register(Agent)
+
